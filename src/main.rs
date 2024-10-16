bbb2908ee4c75aa2e8b9f1c084ff5544de61b646
Giacomo Stevanato <giaco.stevanato@gmail.com>
2024-10-16 12:43:51 +0200

    Add graceful shutdown

--- src/main.rs
+++ src/main.rs
@@ -76,5 +76,35 @@ async fn main() {
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind((IP, PORT)).await.unwrap();
-    axum::serve(listener, app).await.unwrap();
+
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    use tokio::signal;
+
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
 }
